b1e92ca8e1db16cdc40c886d3bab65a9dfcb7626
ZhengshuaiJ <277051210@qq.com>
2024-07-29 20:32:20 +0800

    Update generate_type1_data.R
    change ’type‘ to prefix

--- R/generate_type1_data.R
+++ R/generate_type1_data.R
@@ -5,25 +5,25 @@
 #' @param meta_data_file Path to the metadata file.
 #' @param gene_count_file Path to the gene count file.
 #' @param selected_clusters A named vector specifying the clusters to include.
-#' @param type Prefix for the output CSV files.
+#' @param prefix Prefix for the output CSV files.
 #' @return NULL
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' type = "type_one"
+#' prefix = "type_one"
 #' meta_data_file <- system.file(package="SSTD", "extdata", "meta_data.csv")
 #' gene_count_file <- system.file(package="SSTD", "extdata", "gene_count.csv")
 #' selected_clusters <- c(celltype1="eL4", celltype2="eL5", celltype3="eL6", celltype4="Endo")
 #' generate_type1_data(meta_data_file, gene_count_file, selected_clusters, "type_one")
 #' }
 #'
-generate_type1_data <- function(meta_data_file, gene_count_file, selected_clusters, type) {
-  org_data <- org_data_space("meta_data.csv", "gene_count.csv", output_csv_file = paste0(type, "_org_data_space.csv"), selected_clusters)
+generate_type1_data <- function(meta_data_file, gene_count_file, selected_clusters, prefix) {
+  org_data <- org_data_space("meta_data.csv", "gene_count.csv", output_csv_file = paste0(prefix, "_org_data_space.csv"), selected_clusters)
 
-  sim_spatial_spot_true_prop_data <- spatial_spot_true_prop(selected_clusters, filename = paste0(type, "_spatial_spot_true_prop.csv"))
+  sim_spatial_spot_true_prop_data <- spatial_spot_true_prop(selected_clusters, filename = paste0(prefix, "_spatial_spot_true_prop.csv"))
 
-  spatial_spot_loc(filename = paste0(type, "_spatial_spot_loc.csv"))
+  spatial_spot_loc(filename = paste0(prefix, "_spatial_spot_loc.csv"))
 
   # Generate gradation arrays for each cell type
   # celltype1
@@ -35,34 +35,34 @@ generate_type1_data <- function(meta_data_file, gene_count_file, selected_cluste
   array <- get_gradation_2d(0, 0,  10, 20, TRUE)
   array_0 <- get_gradation_2d(1, 1,  10, 20, TRUE)
   array_1 <- get_gradation_2d(0, 0,  20, 20, TRUE)
-  get_gradation_2d_celltype2 <- rbind(array, array_0)
-  get_gradation_2d_celltype2 <- rbind(get_gradation_2d_celltype2, array_1)
+  get_gradation_2d_cell2 <- rbind(array, array_0)
+  get_gradation_2d_cell2 <- rbind(get_gradation_2d_cell2, array_1)
 
-  # celltype3
+  # cell3
   array <- get_gradation_2d(0, 0,  10, 20, TRUE)
   array_0 <- get_gradation_2d(1, 1,  10, 20, TRUE)
   array_1 <- get_gradation_2d(0, 0,  20, 20, TRUE)
-  get_gradation_2d_celltype3 <- rbind(array_1, array_0)
-  get_gradation_2d_celltype3 <- rbind(get_gradation_2d_celltype3, array)
+  get_gradation_2d_cell3 <- rbind(array_1, array_0)
+  get_gradation_2d_cell3 <- rbind(get_gradation_2d_cell3, array)
 
-  # celltype4
+  # cell4
   array <- get_gradation_2d(1, 1,  10, 20, TRUE)
   array_1 <- get_gradation_2d(0, 0,  30, 20, TRUE)
-  get_gradation_2d_celltype4 <- rbind(array_1, array)
+  get_gradation_2d_cell4 <- rbind(array_1, array)
 
-  result_matrices <- list(result_matrix(get_gradation_2d_celltype1),
-                          result_matrix(get_gradation_2d_celltype2),
-                          result_matrix(get_gradation_2d_celltype3),
-                          result_matrix(get_gradation_2d_celltype4))
+  result_matrices <- list(result_matrix(get_gradation_2d_cell1),
+                          result_matrix(get_gradation_2d_cell2),
+                          result_matrix(get_gradation_2d_cell3),
+                          result_matrix(get_gradation_2d_cell4))
 
-  new_data <- spatial_spot_nUMI(sim_spatial_spot_true_prop_data, result_matrices, selected_clusters, org_data, type)
+  new_data <- spatial_spot_nUMI(sim_spatial_spot_true_prop_data, result_matrices, selected_clusters, org_data, )
 
   combined_arrays <- list(selected_clusters, get_gradation_2d_celltype1,
                           get_gradation_2d_celltype2,
                           get_gradation_2d_celltype3,
                           get_gradation_2d_celltype4)
 
-  generate_plots_and_save(combined_arrays, selected_clusters, new_data, sim_spatial_spot_true_prop_data, type)
+  generate_plots_and_save(combined_arrays, selected_clusters, new_data, sim_spatial_spot_true_prop_data, prefix)
 
   return(NULL)
 }
